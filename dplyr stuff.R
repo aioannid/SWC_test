c554feb0640f8b61e51e3f61a5a701c8a8a8d9c6
Angelos-Gerasimos Ioannidis <Angelos.Ioannidis@UGent.be>
2017-09-15 16:29:00 +0200

    next things i didn't get

--- dplyr stuff.R
+++ dplyr stuff.R
@@ -38,4 +38,16 @@ physicochem <- df %>%
     select(sample_title, temp, ph, Conductivity)
         diversity <- df %>%
              select(sample_title, contains("Diversity"))
+ #join data sets       
+        meanph <- df %>%
+          filter(Reactor.cycle == 2) %>% 
+          group_by(Reactor.phase) %>%
+          mutate(condratio=Conductivity/temp) %>% 
+          summarise(mean.ph = mean(ph),
+                    mean.d2 = mean(Diversity...D2),
+                    sd.ph = sd(ph),
+                    sd.d2 = sd(Diversity...D2),
+                    avelog10.celldens = mean(log10(Cell.density..cells.mL.)),
+                    mean.condrat = mean(condratio))
+        
   
